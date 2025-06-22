ebca3ad63887317610cbbd181642508a4e46294e
kokuto821 <82218969+kokuto821@users.noreply.github.com>
2025-06-22 17:47:44 +0900

    refactor: fetchAllPostsの引数を削除し、URLを固定化

--- src/utils/fetchAllPosts.ts
+++ src/utils/fetchAllPosts.ts
@@ -1,14 +1,8 @@
 import { EMBED, POSTS_URL } from '../constants/urls';
 import type { WordPressPostDetail } from '../types/type';
 
-type Props = {
-  url: string | null;
-};
-
-export const fetchAllPosts = async (
-  { url }: Props = { url: null }
-): Promise<WordPressPostDetail[]> => {
-  const fetchUrl = url ? url : `${POSTS_URL}${EMBED}`;
+export const fetchAllPosts = async (): Promise<WordPressPostDetail[]> => {
+  const fetchUrl = `${POSTS_URL}${EMBED}`;
   if (!fetchUrl) {
     return fetch(`${POSTS_URL}${EMBED}`).then(res => res.json());
   }
